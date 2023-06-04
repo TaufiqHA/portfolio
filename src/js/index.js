4734cbf10d5fe83c845b1cc8b6d5b39e3759cc66
TaufiqHA <htaufiq225@gmail.com>
2023-06-04 14:54:42 +0800

    add resonsive navbar

--- src/js/index.js
+++ src/js/index.js
@@ -9,3 +9,17 @@ function listActive() {
 navList.forEach((item) => {
   item.addEventListener("click", listActive);
 });
+
+// navbar
+
+const nav = document.getElementById("navbar");
+window.addEventListener("scroll", () => {
+  let scrollY = 200;
+  let windwoPosotion = window.pageYOffset;
+
+  if (windwoPosotion >= scrollY) {
+    nav.classList.add("active");
+  } else {
+    nav.classList.remove("active");
+  }
+});
